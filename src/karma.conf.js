58a9a7f282f2fc6fce979f70039665099a761f1e
mohdz1985 <42806226+mohdz1985@users.noreply.github.com>
2018-11-21 10:46:21 +0530

    CI Setting
    CI Setting

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,6 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-teamcity-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -19,6 +20,9 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
+    },    
+    angularcli :{
+      envoirnment : 'dev'
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
